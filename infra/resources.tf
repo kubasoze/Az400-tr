d8a50312cf3fbb3abf14edc0dea92e264b578a80
JS <js@js.com>
2021-04-29 14:28:59 +0200

    zd3v1

--- infra/resources.tf
+++ infra/resources.tf
@@ -20,7 +20,14 @@ resource "azurerm_app_service" "az400app_servicejs" {
   resource_group_name = azurerm_resource_group.az400terraform.name
   app_service_plan_id = azurerm_app_service_plan.az400serviceplanjs.id
 
-  #  app_settings = {
-  #   "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app_insigts.instrumentation_key
-  # }
+   app_settings = {
+    "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.az400insigtsjs.instrumentation_key
+  }
+}
+
+resource "azurerm_application_insights" "az400insigtsjs" {
+  name                = "tf-test-appinsightsjs"
+  location            = azurerm_resource_group.az400terraform.location
+  resource_group_name = azurerm_resource_group.az400terraform.name
+  application_type    = "web"
 }
