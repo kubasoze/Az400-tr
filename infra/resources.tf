f43f934cc8f417c7080cf6d16513a6c1638b51f1
JS <js@js.com>
2021-04-29 14:03:41 +0200

    zd2v3

--- infra/resources.tf
+++ infra/resources.tf
@@ -18,7 +18,7 @@ resource "azurerm_app_service" "az400app_servicejs" {
   name                = "WebApp-az400js"
   location            = azurerm_resource_group.az400terraform.location
   resource_group_name = azurerm_resource_group.az400terraform.name
-  app_service_plan_id = azurerm_app_service_plan.az400serviceplan.id
+  app_service_plan_id = azurerm_app_service_plan.az400serviceplanjs.id
 
   #  app_settings = {
   #   "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.app_insigts.instrumentation_key
