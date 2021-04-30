78bde21e147a0b5c1e3150f331adc88b7cbe075e
JS <js@js.com>
2021-04-30 10:24:21 +0200

    zd5v2

---
+++ infra/data.tf
@@ -0,0 +1,4 @@
+data "azurerm_container_registry" "spark_registry" {
+  name                = "400azureterraform"
+  resource_group_name = "az400"
+}
--- infra/resources.tf
+++ infra/resources.tf
@@ -23,6 +23,16 @@ resource "azurerm_app_service" "az400app_servicejs" {
    app_settings = {
     "APPINSIGHTS_INSTRUMENTATIONKEY" = azurerm_application_insights.az400insigtsjs.instrumentation_key
   }
+   site_config {
+    linux_fx_version = "DOCKER|appsvcsample/python-helloworld:0.1.2"
+  }
+}
+
+resource "azurerm_app_service" "az400app_service_codejs" {
+  name                = "WebApp-az400-codejs"
+  location            = azurerm_resource_group.az400terraform.location
+  resource_group_name = azurerm_resource_group.az400terraform.name
+  app_service_plan_id = azurerm_app_service_plan.az400serviceplanjs.id
 }
 
 resource "azurerm_application_insights" "az400insigtsjs" {
